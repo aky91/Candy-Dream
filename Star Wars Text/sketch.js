ae37974d3a69bb0aef84e0316660b2e669ee5155
AKASH YADAV <aakash28399@gmail.com>
2018-05-22 23:42:37 +0530

    Add files via upload

---
+++ Star Wars Text/sketch.js
@@ -0,0 +1,14 @@
+
+function setup() {
+    
+    //making canvas of 600x600
+    createCanvas(600, 600);
+    
+}
+
+function draw() {
+    
+    //coloring background 
+    background(51);
+    
+}
